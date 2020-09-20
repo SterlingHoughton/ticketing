cf67ae9af62989be160e12bd5ee18364c22c2e81
sterlinghoughton <sterling@sterlinghoughtons-iMac.local>
2020-09-20 12:42:07 -0700

    create BadRequestError handler, update signup file.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -27,8 +28,7 @@ router.post(
     const exisitingUser = await User.findOne({ email });
 
     if (exisitingUser) {
-      console.log('Email in use');
-      return res.send({});
+      throw new BadRequestError(`${exisitingUser.email} is already in use.`)
     }
 
     const user = User.build({ email, password });
