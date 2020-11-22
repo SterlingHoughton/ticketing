a1487bbf9ed02fe34bce0e2e938b9a9c039929af
Sterling Houghton <sterling.houghton@shoughton.lan>
2020-11-22 14:33:01 -0800

    create signup page

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    
+    const response = await axios.post('/api/users/signup', {
+      email, password
+    });
+
+    console.log(response.data);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input 
+          value={email} 
+          onChange={event => setEmail(event.target.value)} 
+          className="form-control"/>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input 
+          value={password} 
+          onChange={event => setPassword(event.target.value)} 
+          type="password" 
+          className="form-control"/>
+      </div>
+      <button className="btn btn-primary">Sign Up</button>
+    </form>
+  );
+};
