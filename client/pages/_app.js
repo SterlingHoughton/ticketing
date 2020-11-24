0f6dfe5c7af4cfb244e9bbea4ce45ba481e9df00
Sterling Houghton <sterling.houghton@shoughton.lan>
2020-11-23 17:27:53 -0800

    configure getInitialProps call for both the AppComponent and any pages/components rendered within it.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const mainApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  
+  return (
+    <div>
+      <h1>{currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+    
+  )
 };
 
-export default mainApp;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+  }
+  
+  return {
+    pageProps,
+    ...data
+  }
+};
+
+export default AppComponent;
