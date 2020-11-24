a9aeb3417557276b6c80af26c75f1ef1c69a32d1
Sterling Houghton <sterling.houghton@shoughton.lan>
2020-11-23 18:14:33 -0800

    create signout page, update signin page function name

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-const signUpPage = () => {
+const signInPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { doRequest, errors } = useRequest ({
@@ -44,4 +44,4 @@ const signUpPage = () => {
   );
 };
 
-export default signUpPage;
+export default signInPage;
