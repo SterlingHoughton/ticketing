943e465d2fe8457cd589a81d3950d25b02f29ae7
Sterling Houghton <sterling.houghton@shoughton.lan>
2020-11-25 13:58:34 -0800

    copy structure and files from auth service, remove any auth service references

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,57 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+// tell TS that there is a global property called authSignup - which is a function that returns a promise
+// that will resolve with a value of type array of string
+declare global {
+  namespace NodeJS {
+    interface Global {
+      authSignup(): Promise<string[]>
+    }
+  }
+}
+
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdf';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.authSignup = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
