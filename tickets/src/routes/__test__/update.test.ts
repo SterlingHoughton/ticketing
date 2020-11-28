c791cf603972c6853c0829b551f3b04df5d268de
Sterling Houghton <sterling.houghton@shoughton.lan>
2020-11-27 18:58:02 -0800

    create update handler and router, mount router to app, create test cases

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -26,17 +26,79 @@ it('returns a 401 if the user is not authenticated', async () => {
 });
 
 it('returns a 401 if a user does not own the ticket', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'New Concert',
+      price: 20
+    });
   
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', global.signin())
+    .send({
+      title: 'updated Concert',
+      price: 100
+    })
+    .expect(401);
+
 });
 
 it('returns a 400 if a user returns an invalid title or price', async () => {
-  
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'New Concert',
+      price: 20
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Updated Title',
+      price: -10
+    })
+    .expect(400);
 });
 
 it('updates the ticket provided vaild inputs', async () => {
-  
-});
+  const cookie = global.signin();
 
-it('', async () => {
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'new concert',
+      price: 20
+    })
   
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'updated title',
+      price: 100
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual('updated title');
+  expect(ticketResponse.body.price).toEqual(100);
 });
